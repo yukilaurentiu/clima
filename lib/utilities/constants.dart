c36d06354fcbc84f30efcd4a138e49ae32dd101a
yukiko laurentiu <yukikolaurentiu@yukikos-MacBook-Air.local>
2023-06-14 15:34:58 -0600

    Initial commit

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const tempTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 100.0,
+);
+
+const messageTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 60.0,
+);
+
+const buttonTextStyle = TextStyle(
+  fontSize: 30.0,
+  fontFamily: 'Spartan MB',
+);
+
+const conditionTextStyle = TextStyle(
+  fontSize: 100.0,
+);
